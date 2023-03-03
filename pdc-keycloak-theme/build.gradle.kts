f062ef60cc8e62826435b4e1ae6be67f5b7dae0e
Jesse Bickel <bickelj@gmail.com>
2023-03-03 10:23:01 -0600

    Add a Keycloak theme skeleton
    To make it easier to create and deploy a theme, add a directory
    structure with placeholders and an associated build that produces a
    jar file. The directory structure is based on the
    [Maven Standard Layout](https://maven.apache.org/guides/introduction/introduction-to-the-standard-directory-layout.html)
    which is compatible with gradle's default settings combined with the
    structure described in
    [Keycloak's "Deploying themes" documentation](https://www.keycloak.org/docs/latest/server_development/index.html#deploying-themes).
    
    Issue #12 Create a working skeleton for the theme module

---
+++ pdc-keycloak-theme/build.gradle.kts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 Open Tech Strategies, LLC
+ * License: Apache License 2.0
+ */
+plugins {
+    `java-library`
+}
+
+// We expect the current LTS version of the JDK for IDEs, compilation, etc.: 17.
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+// We target the class version of the JRE used in the keycloak container: 11.
+tasks.compileJava {
+    options.release.set(11)
+}
